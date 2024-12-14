f577bd9963769da246aa84630e688b8683fb0f25
NguyenPhamMinhQuan <88914475+NguyenPhamMinhQuan@users.noreply.github.com>
2024-12-14 18:59:44 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://nguyenphamminhquan.github.io',
-  base: '/BizSolWebsite'
+  site: 'https://nguyenphamminhquan.github.io'
 })
